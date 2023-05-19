625571a88b9550f16566a980c31d08fa684d909d
tesfa <tesfafikre08@gmail.com>
2023-05-19 20:26:24 +0300

    chat screen asabedkut may 19

--- lib/src/ui/chat/components/body.dart
+++ lib/src/ui/chat/components/body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:lomi/src/Blocs/AuthenticationBloc/bloc/auth_bloc.dart';
 import 'package:lomi/src/Data/Models/model.dart';
 
@@ -43,38 +44,48 @@ class Body extends StatelessWidget {
                                 child: Container(
                                   padding: EdgeInsets.all(10),
                                   decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.all(Radius.circular(20)),
-                                    color: Colors.green,
+                                    borderRadius: BorderRadius.only(
+                                      bottomLeft: Radius.circular(20),
+                                      topLeft: Radius.circular(20),
+                                      topRight: Radius.circular(20)
+                                      ),
+                                    color: Colors.green.shade200,
                                   ),
                                   child: Text(
                                     state.messages![index].message,                                    
                                     //userMatch.chat![0].messages[index].message,
-                                    style: TextStyle(color: Colors.white),))
+                                    style: Theme.of(context).textTheme.bodySmall!.copyWith(color: Colors.black)
+                                    //TextStyle(color: Colors.black)
+                                    ,))
                                 ) :
           
-                                Row(
-                                  children: [
-                                    CircleAvatar(
+                                // Row(
+                                //   children: [
+                                //     CircleAvatar(
                                       
-                                       backgroundImage: NetworkImage(userMatch.matchedUser.imageUrls[0]),
+                                //        backgroundImage: NetworkImage(userMatch.matchedUser.imageUrls[0]),
                   
-                                         ),
-                                         SizedBox(width: 10,),
+                                //          ),
+                                //          SizedBox(width: 10,),
                                     Align(
                                     alignment: Alignment.topLeft,
                                     child: Container(
                                       padding: EdgeInsets.all(10),
                                       decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.all(Radius.circular(20)),
-                                        color: Colors.grey.shade400,
+                                        borderRadius: BorderRadius.only(
+                                      bottomRight: Radius.circular(20),
+                                      topLeft: Radius.circular(20),
+                                      topRight: Radius.circular(20)
+                                      ),
+                                        color: Colors.grey.shade200,
                                       ),
                                       child: Text(
                                         state.messages![index].message,
                                         //userMatch.chat![0].messages[index].message,
-                                        style: TextStyle(color: Colors.white),))
+                                        style: Theme.of(context).textTheme.bodySmall!.copyWith(color: Colors.black),))
                                     ),
-                                  ],
-                                )
+                                //   ],
+                                // )
           
           
                             );
@@ -84,47 +95,120 @@ class Body extends StatelessWidget {
             ),
             
           ),
+
+
+          
+          
     
     
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 20),
-            //height: 100,
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
+          // Container(
+          //   padding: EdgeInsets.symmetric(horizontal: 20),
+          //   //height: 100,
+          //   child: Row(
+          //     crossAxisAlignment: CrossAxisAlignment.end,
+          //     children: [
     
-                Expanded(
-                  child: Container(
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.grey
-                    ),
-                    child: TextField(
-                      onChanged: (value) {
-                        msg = value;
-                      },
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Colors.white,
-                        hintText: 'Type here...',
-                        contentPadding: const EdgeInsets.only(top: 5, left: 20, bottom: 5 ),
+          //       Expanded(
+          //         child: Container(
+          //           decoration: BoxDecoration(
+          //             shape: BoxShape.circle,
+          //             color: Colors.grey
+          //           ),
+          //           child: TextField(
+          //             onChanged: (value) {
+          //               msg = value;
+          //             },
+          //             decoration: InputDecoration(
+          //               filled: true,
+          //               fillColor: Colors.white,
+          //               hintText: 'Type here...',
+          //               contentPadding: const EdgeInsets.only(top: 5, left: 20, bottom: 5 ),
                         
-                      ),
-                      maxLines: 6,
-                      minLines: 1,
-                    ),
-                  ),
-                ),
+          //             ),
+          //             maxLines: 6,
+          //             minLines: 1,
+          //           ),
+          //         ),
+          //       ),
     
-                IconButton(
-                  onPressed: (){
-                    context.read<ChatBloc>().add(SendMessage(message: Message(id: 'id', senderId: context.read<AuthBloc>().state.user!.uid, receiverId: context.read<AuthBloc>().state.user!.uid == state.messages![0].senderId ? state.messages![0].receiverId : state.messages![0].senderId, message: msg, timestamp: DateTime.now())));
-                  }, 
-                  icon: Icon(Icons.send_rounded, color: Colors.green,))
-              ],
-            ),
+          //       IconButton(
+          //         onPressed: (){
+          //           context.read<ChatBloc>().add(SendMessage(message: Message(id: 'id', senderId: context.read<AuthBloc>().state.user!.uid, receiverId: context.read<AuthBloc>().state.user!.uid == state.messages![0].senderId ? state.messages![0].receiverId : state.messages![0].senderId, message: msg, timestamp: DateTime.now())));
+          //         }, 
+          //         icon: Icon(Icons.send_rounded, color: Colors.green,))
+          //     ],
+          //   ),
               
+          //   )
+
+          Container(
+      height: 60,
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.only(left: 20, right: 20),
+        child: Row(
+          children: [
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              height: 40,
+              width: MediaQuery.of(context).size.width/1.3,
+              decoration: BoxDecoration(
+                color: Colors.grey.shade200,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(20)
+                ),
+              ),
+              child:  TextField(
+                onChanged: (value){
+                  msg = value;
+                },
+                cursorColor: Colors.black,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: 'Type Here!',
+                  //hintStyle: Theme.of(context).textTheme.bodySmall,
+                  icon: Icon(
+                    FontAwesomeIcons.faceSmile, 
+                    color: Colors.grey,
+                    size: 20,
+                    ),
+                    suffix: Icon(
+                      FontAwesomeIcons.paperclip,
+                      color: Colors.grey,
+                      size: 20,
+                    )
+                ),
+              ),
+            ),
+
+            SizedBox(width: 7,),
+
+            Container(
+              padding: EdgeInsets.only(right: 5),
+              height: 40,
+              width: 40,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.green,
+              ),
+              child: IconButton(
+                icon: Icon(FontAwesomeIcons.paperPlane,
+                color: Colors.white,
+                size: 20,
+                ),
+                onPressed: ()async{
+                  msg='';
+                  context.read<ChatBloc>().add(SendMessage(message: Message(id: 'id', senderId: context.read<AuthBloc>().state.user!.uid, receiverId: context.read<AuthBloc>().state.user!.uid == state.messages![0].senderId ? state.messages![0].receiverId : state.messages![0].senderId, message: msg, timestamp: DateTime.now())));
+                },
+              ),
             )
+          ],
+      
+        ),
+      ),
+    )
+
+            
         ],
       );
         }else{

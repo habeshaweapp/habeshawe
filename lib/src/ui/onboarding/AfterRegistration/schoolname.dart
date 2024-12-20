2032270bec7273a485a10d021ef48511d63df471
userC10 <tesfafikre08@gmail.com>
2024-12-20 21:16:18 +0300

    Update schoolname.dart

--- lib/src/ui/onboarding/AfterRegistration/schoolname.dart
+++ lib/src/ui/onboarding/AfterRegistration/schoolname.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
